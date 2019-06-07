a3cda558e0a71a8ea75c52104514d6aa9b5d4d8f
Eldar Shalev <eldarshalev1@gmail.com>
2019-06-07 15:11:29 +0300

    Init w1,w2,b1,b2

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import sys
 import numpy as np
 import random
 
+# Global params
 sizeOfTrain = 0.8
+InitH = 12  # between 10-784
+LRT = 0.1  # learning rate between 0.0001, 0.001, 0.01 ...
+input_size = 28 * 28
+output_size = 10
 
 
 def shuffleAllData(x, y):
@@ -10,6 +15,11 @@ def shuffleAllData(x, y):
     random.shuffle(zipped)
     new_set_x, new_set_y = zip(*zipped)
     return new_set_x, new_set_y
+
+
+def initialize_weights(H_size, DataSet, I_size=1):
+    matrix = np.zeros(shape=(H_size,I_size))
+    return matrix
 
 
 def main():
@@ -25,9 +35,16 @@ def main():
     # Shuffle and split data to train and test
     DataSet_X, DataSet_Y = shuffleAllData(DataSet_X, DataSet_Y)
     trainSize = len(DataSet_X) * sizeOfTrain
-    trainSize=int(trainSize)
+    trainSize = int(trainSize)
     dev_x, dev_y = DataSet_X[:trainSize], DataSet_Y[:trainSize]
     test_x, text_y = DataSet_X[trainSize:], DataSet_Y[trainSize:]
+
+    # Declerations of Hyper-Params
+    hidden_size = InitH
+
+    W1, b1 = initialize_weights(hidden_size, DataSet_X, input_size), initialize_weights(hidden_size, DataSet_X)
+    W2, b2 = initialize_weights(output_size, DataSet_Y, hidden_size), initialize_weights(output_size, DataSet_Y)
+   # w1 = w1 - LRT * w1_new
 
 
 if __name__ == '__main__':
